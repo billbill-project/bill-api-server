239bb9362c68f870af5233d694b85d5afffb41c4
Kim Seongjin <darkmoon625@naver.com>
2024-09-16 22:52:01 +0900

    Initialize project

---
+++ src/main/java/site/billbill/billbill/BillbillApplication.java
@@ -0,0 +1,13 @@
+package site.billbill.billbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillbillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/billbill/billbill/BillbillApplicationTests.java
@@ -0,0 +1,13 @@
+package site.billbill.billbill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillbillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
