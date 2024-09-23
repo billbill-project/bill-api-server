38c850f646c8e44bda7c07a6f3d29a862635524d
Kim Seongjin <darkmoon625@naver.com>
2024-09-24 00:03:41 +0900

    Setting EntityScan & ConfigScan

--- src/main/java/site/billbill/apiserver/BillApiServerApplication.java
+++ src/main/java/site/billbill/apiserver/BillApiServerApplication.java
@@ -2,13 +2,17 @@ package site.billbill.apiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EntityScan(basePackages = "site.billbill.apiserver.model")
+@ConfigurationPropertiesScan("site.billbill.apiserver.config")
 public class BillApiServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BillApiServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BillApiServerApplication.class, args);
+    }
 
 }
