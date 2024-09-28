4268cde9597994fbddd1a3a08bb2f394ab076bd2
Kim Seongjin <darkmoon625@naver.com>
2024-09-28 14:59:24 +0900

    BaseResponse 수정

--- src/main/java/site/billbill/apiserver/common/response/BaseResponse.java
+++ src/main/java/site/billbill/apiserver/common/response/BaseResponse.java
@@ -1,10 +1,16 @@
 package site.billbill.apiserver.common.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.time.OffsetDateTime;
 
 @Data
 public class BaseResponse {
     private boolean isSuccess;
+    private String code = "SUCCESS";
     private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private OffsetDateTime responseAt = OffsetDateTime.now();
     private Object data;
 }
