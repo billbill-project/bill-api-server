3ee5b16cfbb89031453cc19584f316da8c5d8049
Kim Seongjin <darkmoon625@naver.com>
2024-09-29 20:20:04 +0900

    Swagger Config 적용

--- src/main/java/site/billbill/apiserver/BillApiServerApplication.java
+++ src/main/java/site/billbill/apiserver/BillApiServerApplication.java
@@ -1,5 +1,7 @@
 package site.billbill.apiserver;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,6 +11,12 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EntityScan(basePackages = "site.billbill.apiserver.model")
 @ConfigurationPropertiesScan("site.billbill.apiserver.config")
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://api.billbill.site", description = "dev server"),
+                @Server(url = "http://localhost:8080", description = "local server"),
+        }
+)
 public class BillApiServerApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/site/billbill/apiserver/config/swagger/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package site.billbill.apiserver.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "authorization";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(
+                                SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("BillBill API")
+                .description("BillBill API 입니다.")
+                .version("1.0.0");
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+//    @Bean
+//    public GroupedOpenApi privateApi() {
+//        return GroupedOpenApi.builder()
+//                .group("external")
+//                .pathsToMatch("/external/**")
+//                .build();
+//    }
+}
