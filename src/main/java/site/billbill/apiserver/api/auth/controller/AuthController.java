6fabccac9d363d34d2586ba69a404f2b1a6bdbbe
Kim Seongjin <darkmoon625@naver.com>
2024-10-05 15:48:44 +0900

    [fix] Swagger 공통 response 부분 수정

--- src/main/java/site/billbill/apiserver/api/auth/controller/AuthController.java
+++ src/main/java/site/billbill/apiserver/api/auth/controller/AuthController.java
@@ -20,27 +20,17 @@ import site.billbill.apiserver.common.utils.jwt.dto.JwtDto;
 @RestController
 @Tag(name = "Auth", description = "Auth API")
 @RequestMapping("/api/v1/auth")
-@ApiResponses(value = {
-        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Server Error", content = @Content)
-})
+//@ApiResponses(value = {
+//        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+//        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+//        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+//        @ApiResponse(responseCode = "500", description = "Server Error", content = @Content)
+//})
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
     @Operation(summary = "회원 가입(일반)", description = "일반 회원 가입 API")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "201",
-                    description = "Created",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = BaseResponse.class)
-                    )
-            ),
-    })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     public BaseResponse<JwtDto> signup(
--- src/main/java/site/billbill/apiserver/common/response/BaseResponse.java
+++ src/main/java/site/billbill/apiserver/common/response/BaseResponse.java
@@ -14,12 +14,12 @@ import java.time.OffsetDateTime;
 public class BaseResponse<T> {
     @Schema(description = "성공 여부", example = "true")
     private boolean isSuccess = true;
-    @Schema(description = "응답 코드", example = "SUCCESS")
-    private String code = "SUCCESS";
-    @Schema(description = "응답 메세지", example = "string")
-    private String message;
-    @Schema(description = "응답일시", example = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Schema(description = "응답 코드", example = "200")
+    private String code = "200";
+    @Schema(description = "응답 메세지", example = "SUCCESS")
+    private String message = "SUCCESS";
+    @Schema(description = "응답 일시", type = "string", format = "date-time", example = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private OffsetDateTime responseAt = OffsetDateTime.now();
     @Schema(description = "응답 데이터")
     private T data;
