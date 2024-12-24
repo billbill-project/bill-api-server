254bcaa48faebeb24e8e15e60cefa438ad2deb3a
jimmy0524 <10jmin04@naver.com>
2024-12-24 21:19:29 +0900

    [feat] 대여날짜 수정 api 구현

--- src/main/java/site/billbill/apiserver/api/chat/service/ChatService.java
+++ src/main/java/site/billbill/apiserver/api/chat/service/ChatService.java
@@ -2,6 +2,7 @@ package site.billbill.apiserver.api.chat.service;
 
 import java.util.List;
 import site.billbill.apiserver.api.chat.dto.request.ChatRequest;
+import site.billbill.apiserver.api.chat.dto.request.ChatRequest.changeDate;
 import site.billbill.apiserver.api.chat.dto.response.ChatResponse.ViewChannelInfoResponse;
 import site.billbill.apiserver.api.chat.dto.response.ChatResponse.ViewChatInfoResponse;
 
@@ -13,4 +14,6 @@ public interface ChatService {
     String startChannel(ChatRequest.borrowInfo request, String userId);
 
     List<ViewChatInfoResponse> getChatList(String beforeTimestamp, String userId);
+
+    String changeDate(String userId, String channelId, changeDate request);
 }
