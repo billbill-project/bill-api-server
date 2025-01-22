cb550e8c3a76aa29975082fd2415746924ea167a
jimmy0524 <10jmin04@naver.com>
2025-01-23 02:14:16 +0900

    [fix] PushRequest -> PushRequest.SendPushRequest 수정

--- src/main/java/site/billbill/apiserver/api/push/service/PushService.java
+++ src/main/java/site/billbill/apiserver/api/push/service/PushService.java
@@ -1,18 +1,16 @@
 package site.billbill.apiserver.api.push.service;
 
-import site.billbill.apiserver.api.push.dto.request.PushRequest;
-
-import java.io.IOException;
 import site.billbill.apiserver.api.push.dto.request.PushRequest.SendChatPushRequest;
 import site.billbill.apiserver.api.push.dto.request.PushRequest.SendPushRequest;
-
 import site.billbill.apiserver.api.push.dto.response.PushResponse;
 import site.billbill.apiserver.api.push.dto.response.PushResponse.GetPushListResponse;
 
+import java.io.IOException;
+
 public interface PushService {
-    boolean sendPush(PushRequest.SendPushRequest request) throws IOException;
+    boolean sendPush(SendPushRequest request) throws IOException;
+
     SendPushRequest sendChatPush(SendChatPushRequest request);
-    boolean sendPush(PushRequest request) throws IOException;
 
     GetPushListResponse getPushList(String beforeTimestamp, String userId);
 
